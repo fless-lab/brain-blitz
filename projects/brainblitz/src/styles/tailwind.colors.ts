2fd8da52bf3b225e0af4762dd402b87431d4f51e
Abdou-Raouf ATARMLA <achilleatarmla@gmail.com>
2024-08-10 22:05:41 +0000

    [CHORE] Configuring theming

--- projects/brainblitz/src/styles/tailwind.colors.ts
+++ projects/brainblitz/src/styles/tailwind.colors.ts
@@ -1,15 +1,117 @@
 const colors = {
-  light: {
-    bg: 'var(--light-bg)',
-    text: 'var(--light-text)',
-    primary: 'var(--light-primary)',
-    secondary: 'var(--light-secondary)',
+  primary: {
+    light: 'var(--light-primary)',
+    DEFAULT: 'var(--light-primary)',
+    dark: 'var(--dark-primary)',
+    ocean: 'var(--ocean-primary)',
   },
-  dark: {
-    bg: 'var(--dark-bg)',
-    text: 'var(--dark-text)',
-    primary: 'var(--dark-primary)',
-    secondary: 'var(--dark-secondary)',
+  'primary-foreground': {
+    light: 'var(--light-primary-foreground)',
+    DEFAULT: 'var(--light-primary-foreground)',
+    dark: 'var(--dark-primary-foreground)',
+    ocean: 'var(--ocean-primary-foreground)',
+  },
+  secondary: {
+    light: 'var(--light-secondary)',
+    DEFAULT: 'var(--light-secondary)',
+    dark: 'var(--dark-secondary)',
+    ocean: 'var(--ocean-secondary)',
+  },
+  'secondary-foreground': {
+    light: 'var(--light-secondary-foreground)',
+    DEFAULT: 'var(--light-secondary-foreground)',
+    dark: 'var(--dark-secondary-foreground)',
+    ocean: 'var(--ocean-secondary-foreground)',
+  },
+  background: {
+    light: 'var(--light-bg)',
+    DEFAULT: 'var(--light-bg)',
+    dark: 'var(--dark-bg)',
+    ocean: 'var(--ocean-bg)',
+  },
+  text: {
+    light: 'var(--light-text)',
+    DEFAULT: 'var(--light-text)',
+    dark: 'var(--dark-text)',
+    ocean: 'var(--ocean-text)',
+  },
+  accent: {
+    light: 'var(--light-accent)',
+    DEFAULT: 'var(--light-accent)',
+    dark: 'var(--dark-accent)',
+    ocean: 'var(--ocean-accent)',
+  },
+  'accent-foreground': {
+    light: 'var(--light-accent-foreground)',
+    DEFAULT: 'var(--light-accent-foreground)',
+    dark: 'var(--dark-accent-foreground)',
+    ocean: 'var(--ocean-accent-foreground)',
+  },
+  destructive: {
+    light: 'var(--light-destructive)',
+    DEFAULT: 'var(--light-destructive)',
+    dark: 'var(--dark-destructive)',
+    ocean: 'var(--ocean-destructive)',
+  },
+  'destructive-foreground': {
+    light: 'var(--light-destructive-foreground)',
+    DEFAULT: 'var(--light-destructive-foreground)',
+    dark: 'var(--dark-destructive-foreground)',
+    ocean: 'var(--ocean-destructive-foreground)',
+  },
+  muted: {
+    light: 'var(--light-muted)',
+    DEFAULT: 'var(--light-muted)',
+    dark: 'var(--dark-muted)',
+    ocean: 'var(--ocean-muted)',
+  },
+  'muted-foreground': {
+    light: 'var(--light-muted-foreground)',
+    DEFAULT: 'var(--light-muted-foreground)',
+    dark: 'var(--dark-muted-foreground)',
+    ocean: 'var(--ocean-muted-foreground)',
+  },
+  card: {
+    light: 'var(--light-card)',
+    DEFAULT: 'var(--light-card)',
+    dark: 'var(--dark-card)',
+    ocean: 'var(--ocean-card)',
+  },
+  'card-foreground': {
+    light: 'var(--light-card-foreground)',
+    DEFAULT: 'var(--light-card-foreground)',
+    dark: 'var(--dark-card-foreground)',
+    ocean: 'var(--ocean-card-foreground)',
+  },
+  popover: {
+    light: 'var(--light-popover)',
+    DEFAULT: 'var(--light-popover)',
+    dark: 'var(--dark-popover)',
+    ocean: 'var(--ocean-popover)',
+  },
+  'popover-foreground': {
+    light: 'var(--light-popover-foreground)',
+    DEFAULT: 'var(--light-popover-foreground)',
+    dark: 'var(--dark-popover-foreground)',
+    ocean: 'var(--ocean-popover-foreground)',
+  },
+  border: {
+    light: 'var(--light-border)',
+    DEFAULT: 'var(--light-border)',
+    dark: 'var(--dark-border)',
+    ocean: 'var(--ocean-border)',
+  },
+  input: {
+    light: 'var(--light-input)',
+    DEFAULT: 'var(--light-input)',
+    dark: 'var(--dark-input)',
+    ocean: 'var(--ocean-input)',
+  },
+  ring: {
+    light: 'var(--light-ring)',
+    DEFAULT: 'var(--light-ring)',
+    dark: 'var(--dark-ring)',
+    ocean: 'var(--ocean-ring)',
   },
 };
 
--- projects/brainblitz/tailwind.config.js
+++ projects/brainblitz/tailwind.config.js
@@ -1,14 +1,26 @@
 import colors from './src/styles/tailwind.colors';
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  darkMode: 'class',
   content: [
-    "./src/**/*.{html,ts,scss}",
+    "./src/**/*.{html,ts,js,scss}",
   ],
   theme: {
     extend: {
       colors: colors,
+      fontFamily: {
+        heading: ['var(--font-heading)', ...fontFamily.sans],
+        body: ['var(--font-body)', ...fontFamily.mono],
+      },
+      borderRadius: {
+        xl: `calc(var(--radius) + 4px)`,
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: `calc(var(--radius) - 4px)`
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-animate'),
+  ],
 };

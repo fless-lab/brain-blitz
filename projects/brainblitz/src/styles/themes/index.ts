4be475d3d666b9281726888cc58995e6e047c1c8
Abdou-Raouf ATARMLA <achilleatarmla@gmail.com>
2024-08-11 01:54:14 +0000

    [UPDATE] Theming using tw-colors

---
+++ projects/brainblitz/src/styles/themes/index.ts
@@ -0,0 +1,9 @@
+import { lightTheme } from './light';
+import { darkTheme } from './dark';
+import { oceanTheme } from './ocean';
+
+export const themes = {
+  light: lightTheme,
+  dark: darkTheme,
+  ocean: oceanTheme,
+};
--- projects/brainblitz/tailwind.config.js
+++ projects/brainblitz/tailwind.config.js
@@ -1,26 +1,32 @@
-import colors from './src/styles/tailwind.colors';
-import { fontFamily } from "tailwindcss/defaultTheme";
+const { createThemes } = require('tw-colors');
+const { themes } = require('./src/styles/themes');
+const fontFamily = require('tailwindcss/defaultTheme').fontFamily;
 
-export default {
+module.exports = {
   content: [
     "./src/**/*.{html,ts,js,scss}",
   ],
   theme: {
     extend: {
-      colors: colors,
       fontFamily: {
         heading: ['var(--font-heading)', ...fontFamily.sans],
         body: ['var(--font-body)', ...fontFamily.mono],
       },
       borderRadius: {
-        xl: `calc(var(--radius) + 4px)`,
-        lg: `var(--radius)`,
-        md: `calc(var(--radius) - 2px)`,
-        sm: `calc(var(--radius) - 4px)`
+        'none': '0',
+        'sm': '0.125rem',
+        'md': '0.375rem',
+        'lg': '0.5rem',
+        'xl': '0.75rem',
+        '2xl': '1rem',
       },
     },
   },
   plugins: [
     require('tailwindcss-animate'),
+    createThemes(themes, {
+      produceCssVariable: (colorName) => `--rtwc-${colorName}`,
+      defaultTheme: 'light',
+    }),
   ],
 };

4ca50ed2fc83216ac70b5a86aa7c7bc5462d5155
Abdou-Raouf ATARMLA <achilleatarmla@gmail.com>
2024-08-12 08:49:55 +0000

    [FEATURE] RootLayout in place

--- projects/brainblitz/src/app/app.routes.ts
+++ projects/brainblitz/src/app/app.routes.ts
@@ -1,14 +1,20 @@
 import { Routes } from '@angular/router';
+import { RootLayoutComponent } from './shared/components/root-layout/root-layout.component';
 
-export const routes: Routes = [
+
+export const routes: Routes =  [
   {
-    path: "",
-    loadChildren: () =>
-      import('./features/home/home.module').then((m) => m.HomeModule),
-  },
-  {
-    path: "bump",
-    loadChildren: () =>
-      import('./features/bump/bump.module').then((m) => m.BumpModule),
+    path: '',
+    component: RootLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)
+      },
+      {
+        path: 'bump',
+        loadChildren: () => import('./features/bump/bump.module').then(m => m.BumpModule)
+      }
+    ]
   }
 ];
--- projects/brainblitz/src/app/shared/shared.module.ts
+++ projects/brainblitz/src/app/shared/shared.module.ts
@@ -2,18 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThemeSwitcherComponent } from './components/theme-switcher/theme-switcher.component';
 import { SelectComponent } from './components/form/select/select.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RootLayoutComponent } from './components/root-layout/root-layout.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     ThemeSwitcherComponent,
-    SelectComponent
+    SelectComponent,
+    NavbarComponent,
+    FooterComponent,
+    RootLayoutComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     ThemeSwitcherComponent,
-    SelectComponent
+    SelectComponent,
+    NavbarComponent,
+    FooterComponent,
+    RootLayoutComponent
   ]
 })
 export class SharedModule { }
